f983bcfb416852572e325de0cef94dd10f813a70
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2021-05-05 22:36:23 +0900

    REFACT : re package jar

--- MM_ProtoType/src/main/java/com/proto/mm/service/PosterService.java
+++ MM_ProtoType/src/main/java/com/proto/mm/service/PosterService.java
@@ -78,7 +78,7 @@ public class PosterService {
         System.out.println(movieCode);
         Poster poster =	posterRepository.findByMovieCode(movieCode);
 
-        String imgUrl = "http://52.200.16.8:8090/poster/" + poster.getPosterPath();
+        String imgUrl = "52.200.16.8:8090/poster/" + poster.getPosterPath();
         
         String tmp = movie.getMovieTitle();
 		String fileName = tmp.replace(" ", "").replace(":", "_");
