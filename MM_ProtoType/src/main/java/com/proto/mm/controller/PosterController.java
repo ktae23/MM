7bcef3677ff100595921daf9a90a8de992dc4ed7
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2021-05-06 00:14:24 +0900

    REFACT : re package jar

--- MM_ProtoType/src/main/java/com/proto/mm/controller/PosterController.java
+++ MM_ProtoType/src/main/java/com/proto/mm/controller/PosterController.java
@@ -1,5 +1,12 @@
 package com.proto.mm.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.proto.mm.model.Movie;
+import com.proto.mm.model.Poster;
 import com.proto.mm.service.PosterService;
 
 @Controller
@@ -18,9 +27,50 @@ public class PosterController {
 	
 	@GetMapping("download")
     public void downLoad(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		posterService.posterDownload(model, request, response);
-
+			posterService.posterDownload(model, request, response);
+			Poster poster = (Poster) model.getAttribute("poster");
+			Movie movie = (Movie) model.getAttribute("movie");
+			
+			File file = new File("/" + poster.getPosterPath());
+	        
+	        FileInputStream fis = null;
+	        BufferedInputStream bis = null;
+	        ServletOutputStream sos = null;
+	        try {
+	        	fis = new FileInputStream(file);
+	        	bis = new BufferedInputStream(fis);
+	        	sos = response.getOutputStream();
+	        	String tmp = movie.getMovieTitle();
+	        	String fileName = tmp.replace(" ", "").replace(":", "_") + ".png";
+	        	String reFilename = "";
+	        	boolean isMSIE = request.getHeader("user-agent").indexOf("MSIE") != -1 || 
+	        			request.getHeader("user-agent").indexOf("Trident") != -1;
+	        	if(isMSIE) { 
+	        	reFilename = URLEncoder.encode(fileName, "utf-8");
+	        	reFilename = reFilename.replaceAll("\\+", "%20"); 
+	        	}
+	        	else {
+	        		reFilename = new String(fileName.getBytes("utf-8"), "ISO-8859-1");
+	        	} 
+	        	
+	        	response.setContentType("application/octet-stream;charset=utf-8");
+	        	response.addHeader("Content-Disposition", "attachment;filename=\""+reFilename+"\"");
+	        	response.setContentLength((int)file.length());
+	        	
+	        	int read = 0;
+	        	while((read = bis.read()) != -1) {
+	        		sos.write(read); 
+	        		} 
+	        	
+	    	}catch(IOException e) {
+	    		e.printStackTrace(); 
+	    	}finally {
+	    		try {
+	    			sos.close();
+	    			bis.close();
+	    			}catch (IOException e) {
+	    				e.printStackTrace(); }
+			}
     }
 }
 
--- MM_ProtoType/src/main/java/com/proto/mm/service/PosterService.java
+++ MM_ProtoType/src/main/java/com/proto/mm/service/PosterService.java
@@ -57,56 +57,21 @@ public class PosterService {
 		return model;
 	}
 
-	public HttpServletResponse posterDownload(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public Model posterDownload(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
         
         String movieTitle = request.getParameter("movieTitle");
         System.out.println(movieTitle);
+        
         Movie movie = movieRepository.findByMovieTitle(movieTitle);
         BigDecimal movieCode = movie.getMovieCode();
+        
         System.out.println(movieCode);
         Poster poster =	posterRepository.findByMovieCode(movieCode);
         
-        File file = new File("/" + poster.getPosterPath());
-        
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        ServletOutputStream sos = null;
-        try {
-        	fis = new FileInputStream(file);
-        	bis = new BufferedInputStream(fis);
-        	sos = response.getOutputStream();
-        	String tmp =movieTitle;
-        	String fileName = tmp.replace(" ", "").replace(":", "_") + ".png";
-        	String reFilename = "";
-        	boolean isMSIE = request.getHeader("user-agent").indexOf("MSIE") != -1 || 
-        			request.getHeader("user-agent").indexOf("Trident") != -1;
-        	if(isMSIE) { 
-        	reFilename = URLEncoder.encode(fileName, "utf-8");
-        	reFilename = reFilename.replaceAll("\\+", "%20"); 
-        	}
-        	else {
-        		reFilename = new String(fileName.getBytes("utf-8"), "ISO-8859-1");
-        	} 
-        	
-        	response.setContentType("application/octet-stream;charset=utf-8");
-        	response.addHeader("Content-Disposition", "attachment;filename=\""+reFilename+"\"");
-        	response.setContentLength((int)file.length());
-        	
-        	int read = 0;
-        	while((read = bis.read()) != -1) {
-        		sos.write(read); 
-        		} 
-        	
-    	}catch(IOException e) {
-    		e.printStackTrace(); 
-    	}finally {
-    		try {
-    			sos.close();
-    			bis.close();
-    			}catch (IOException e) {
-    				e.printStackTrace(); }
-		}
-		return response;
+        model.addAttribute("poster",poster);
+        model.addAttribute("movie",movie);
+       
+		return model;
 
         
 		/*
