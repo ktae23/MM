6e1c6797ed3a08f60cea06b9a4cce256240c263b
Kyungtae Park <74488924+ktae23@users.noreply.github.com>
2021-05-06 00:40:39 +0900

    REFACT : re package jar

--- MM_ProtoType/src/main/java/com/proto/mm/controller/PosterController.java
+++ MM_ProtoType/src/main/java/com/proto/mm/controller/PosterController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proto.mm.service.PosterService;
 
@@ -16,7 +16,7 @@ public class PosterController {
 	@Autowired
 	PosterService posterService;
 	
-	@GetMapping("download")
+	@RequestMapping("download")
     public void downLoad(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		posterService.posterDownload(model, request, response);
